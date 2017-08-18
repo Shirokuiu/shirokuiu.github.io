e7de446c3bec3397f243820b5f572ae7e7aaba5e
Pavel <shirokuiuna@mail.ru>
2017-08-18 10:51:43 +0300

    upload sedona files

--- sedona/static/js/mainxgbts42h8hlesoamj9k9.js
+++ sedona/static/js/mainxgbts42h8hlesoamj9k9.js
@@ -1,11 +1,20 @@
 var mainNavButton = document.querySelector('.main-nav__button');
 var mainNavBlockWrap = document.querySelector('.main-nav__blockWrap');
 var mainNavList = document.querySelectorAll('.main-nav__list');
+var mainNavButtonClose = document.querySelector('.main-nav__button-close');
 
 mainNavButton.addEventListener('click', function() {
   mainNavBlockWrap.classList.toggle('main-nav__blockWrap--padding-vissible');
   mainNavList[0].classList.toggle('main-nav__list--vissible');
   mainNavList[1].classList.toggle('main-nav__list--vissible');
+  mainNavButtonClose.classList.toggle('main-nav__button-close--vissible');
+});
+
+mainNavButtonClose.addEventListener('click', function() {
+  mainNavBlockWrap.classList.remove('main-nav__blockWrap--padding-vissible');
+  mainNavList[0].classList.remove('main-nav__list--vissible');
+  mainNavList[1].classList.remove('main-nav__list--vissible');
+  mainNavButtonClose.classList.toggle('main-nav__button-close--vissible');
 });
 
 var orderButton = document.querySelector('.order__button');
