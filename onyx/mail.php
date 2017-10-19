b4bbbc0388bf17d8a9bb0a33c0b4d5663bb85b8e
Shirokuiu <shirokuiuna@mail.ru>
2017-10-19 16:13:15 +0300

    upload

---
+++ onyx/mail.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'shirokuiuna@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Новый заказ'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
