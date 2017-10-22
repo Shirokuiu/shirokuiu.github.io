57f4434db0768cdd8ab9412c102c97e6cf015b50
Pavel <shirokuiuna@mail.ru>
2017-10-22 19:33:24 +0300

    upload

--- onyx/mail.php
+++ onyx/mail.php
@@ -1,12 +1,19 @@
-<?php
-
-$recepient = "shirokuiuna@mail.ru";
-$sitename = "Новый заказ";
-
-$name = trim($_POST["name"]);
-$mail = trim($_POST["email"]);
-$tel = trim($_POST["tel"]);
-$message = "Имя: $name \nEmail: $mail \nТелефон: $tel";
-
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")(isset($_POST['tel'])&&$_POST['tel']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'shirokuiuna@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Новый заказ'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
