d2f99e4fa0082731b20d5b45aeaa4f6a838d1d9c
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2018-05-07 23:00:01 +0300

    upload

--- massage/static/js/mainao9qxh.js
+++
@@ -1,8 +0,0 @@
-var pageFooterItemBurger = document.querySelector('.page-footer__item--burger');
-var burgerPopup = document.querySelector('.burger-popup');
-
-var showBurgerPopup = function () {
-  burgerPopup.classList.toggle('burger-popup--hidden');
-};
-
-pageFooterItemBurger.addEventListener('click', showBurgerPopup);
