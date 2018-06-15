d68f3e65a838e8a0f88bbeeabfb3fec2cb16c0d0
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2018-06-15 18:44:33 +0300

    upload

--- panda/static/js/separate-js/news.js
+++ panda/static/js/separate-js/news.js
@@ -2,6 +2,12 @@
   var container = document.querySelector('.news__preview');
   var text = container.querySelector('.news__preview-description');
   var linkDropDown = container.querySelector('.news__preview-description-link-drop-down');
+  var addInterrogation = document.querySelector('.news__block-control-add-interrogation');
+  var addPost = document.querySelector('.news__block-control-add-post');
+  var interrogationBlock = document.querySelector('.news__block-interrogation-block');
+  var postBlock = document.querySelector('.news__block-post-block');
+  var newsSettingsButton = document.querySelector('.news__settings-button');
+  var newsSettings = document.querySelector('.news--settings');
   
   linkDropDown.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -12,5 +18,21 @@
     } else {
       this.textContent = 'Показать полностью...';
     }
+  });
+  
+  addInterrogation.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    
+    interrogationBlock.classList.toggle('hidden');
+  });
+  
+  addPost.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    
+    postBlock.classList.toggle('hidden');
+  });
+  
+  newsSettingsButton.addEventListener('click', function () {
+    newsSettings.classList.remove('hidden');
   });
 })();
