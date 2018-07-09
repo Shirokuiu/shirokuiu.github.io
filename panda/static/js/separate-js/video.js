66586e197ee69aae85ca325eea4fe5d2fdf703b9
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2018-07-09 19:57:27 +0300

    upload

--- panda/static/js/separate-js/video.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var container = document.querySelector('.video');
-  var button = container.querySelector('.video__show-video-button');
-  var videos = container.querySelector('.video__carts');
-  
-  button.addEventListener('click', function () {
-    videos.classList.toggle('video__carts--hidden');
-  });
-})();
